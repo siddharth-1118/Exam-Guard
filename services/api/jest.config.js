7aa970cb5284ec229b4a0eb938ca2a5f301a3040
siddharth-1118 <saisiddharthvooka@gmail.com>
2026-09-05 23:41:23 +0530

    fix(api): add testTimeout=30s and forceExit to prevent CI flake from slow Redis timing tests

--- services/api/jest.config.js
+++ services/api/jest.config.js
@@ -5,6 +5,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   moduleFileExtensions: ['ts', 'js'],
   testRegex: '.*\\.spec\\.ts$',
+  testTimeout: 30000,
+  forceExit: true,
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
   },
